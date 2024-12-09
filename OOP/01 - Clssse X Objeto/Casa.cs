6598e8337f43147d11e44dd1220ba4b86bf53b1c
samuel.alves <samuel.alves@supermercadosbh.com.br>
2024-12-09 13:04:06 -0300

    Diferença entre classe e objeto, e estado e comportamento

---
+++ OOP/01 - Clssse X Objeto/Casa.cs
@@ -0,0 +1,28 @@
+﻿namespace OOP.Clssse_X_Objeto;
+
+
+// Definicação de classe, ela te o objetivo de mapear um objeto do mundo real.
+
+public class Casa
+{
+    public int TamanhoM2 { get; set; }
+    public int Andares { get; set; }
+    public decimal Valor { get; set; }
+    public int NumeroVagas { get; set; }
+}
+
+public class Objeto
+{
+    // E quando você instancia ela na memoria, no código. Um objeto e quando você traz aquela classe para a realidade do código.
+    public Objeto()
+    {
+        var casa = new Casa
+        {
+            TamanhoM2 = 1,
+            Andares = 1,
+            Valor = 1,
+            NumeroVagas = 1
+        };
+    }
+}
+
---
+++ OOP/02 - Pilares OOP/Pessoa_EstadoComportamento.cs
@@ -0,0 +1,27 @@
+﻿namespace OOP._02___Pilares_OOP;
+
+// Estado e entender que as propriedades da classe possui informações, já o comportamento são métodos que processa uma informação.
+public class Pessoa_EstadoComportamento
+{
+    // Isso aqui é estado são as propriedades da própria classe. E quando você tem os dados está armazenado na classe.
+    public string Nome { get; set; }
+    public DateTime DataNascimento { get; set; }
+
+    // E quando você tem uma ação dentro da classe, isso quer dizer que é um comportamento. Não necessariamente temos que retonar pode fazer
+    // com que ele simplemente altere uma informação, como é mostrado no exemplo 2.
+    public int CalcularIdade()
+    {
+        var dataAtual = DateTime.Now;
+        var idade = dataAtual.Year - DataNascimento.Year;
+
+        if (dataAtual.Month < DateTime.Now.Month) idade--;
+
+        return idade;
+    }
+
+    public void Update(string nome, DateTime dataNascimento)
+    {
+        Nome = nome;
+        DataNascimento = dataNascimento;
+    }
+}
