e0caed4212bbe23b2252cb6cbb9d82c78a2b11a6
samuel.alves <samuel.alves@supermercadosbh.com.br>
2024-12-16 13:09:47 -0300

    Estudo de interface e implementacao/ heranca e composicao

---
+++ OOP/03_Interface_Implementacao/Cases.cs
@@ -0,0 +1,61 @@
+﻿namespace OOP._03_Interface_Implementacao;
+
+/// <summary>
+/// A interface é uma espécie de contrato. Isso significa que toda classe que implementa uma interface
+/// é obrigada a implementar os métodos definidos nela.
+/// </summary>
+public interface IRepositorio
+{
+    void Adicionar();
+}
+
+/// <summary>
+/// Neste exemplo, podemos observar que a classe Repositorio precisa implementar o método Adicionar,
+/// pois ele foi definido na interface IRepositorio que está sendo herdada.
+/// </summary>
+public class Repositorio : IRepositorio
+{
+    public void Adicionar()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+/// <summary>
+/// Aqui criamos uma instância da classe Repositorio e chamamos a função Adicionar diretamente.
+/// </summary>
+public class UsoImplementacao
+{
+    public void Processo()
+    {
+        var repository = new Repositorio();
+        repository.Adicionar();
+    }
+}
+
+/// <summary>
+/// Aqui utilizamos a interface como um contrato, permitindo abstrair o uso da classe concreta.
+/// </summary>
+public class UsoAbstracao
+{
+    // O repositório está representando a interface, permitindo maior flexibilidade e desacoplamento.
+    private readonly IRepositorio _repositorio;
+
+    /// <summary>
+    /// Construtor que recebe uma instância de IRepositorio, implementando o padrão de injeção de dependência.
+    /// Esse processo garante que estamos utilizando a interface como contrato para nossas operações.
+    /// </summary>
+    /// <param name="repositorio">Instância que implementa a interface IRepositorio.</param>
+    public UsoAbstracao(IRepositorio repositorio)
+    {
+        _repositorio = repositorio;
+    }
+
+    /// <summary>
+    /// Método que utiliza a função definida no contrato da interface.
+    /// </summary>
+    public void Processo()
+    {
+        _repositorio.Adicionar();
+    }
+}
---
+++ OOP/04_Heranca_Composicao/Cases.cs
@@ -0,0 +1,80 @@
+﻿using OOP._02_Pilares_OOP.EstadoComportamento;
+
+namespace OOP._04_Heranca_Composicao;
+
+#region Caso 1
+
+// Aqui temos um caso de herança.
+public class PessoaFisica : Pessoa
+{
+    public string Cpf { get; set; }
+}
+
+// Aqui temos um caso de Composição.
+public class PessoaFisica2
+{
+    public Pessoa Pessoa { get; set; }
+    public string Cpf { get; set; }
+}
+
+public class TestesHerancaComposicao
+{
+    public TestesHerancaComposicao()
+    {
+        var pessoaHeranca = new PessoaFisica
+        {
+            Nome = "Teste agora",
+            Cpf = "155545454",
+            DataNascimento = DateTime.Now,
+
+        };
+
+        var pessoaComposicao = new PessoaFisica2
+        {
+            Pessoa = new Pessoa
+            {
+                Nome = "Teste Aqui",
+                DataNascimento = DateTime.Now
+            },
+            Cpf = "155151515"
+        };
+    }
+}
+
+#endregion
+
+#region Caso 2 
+
+public interface IRepositorio<T>
+{
+    void Adicionar(T obj);
+}
+
+public interface IRepositorioPessoa
+{
+    void Adicionar(Pessoa pessoa);
+}
+
+public class Repositorio<T> : IRepositorio<T>
+{
+    public void Adicionar(T obj)
+    {
+
+    }
+}
+
+public class RepositorioPessoaHeranca : Repositorio<Pessoa>, IRepositorioPessoa
+{
+
+};
+
+public class RepositorioPessoaComposicao : IRepositorioPessoa
+{
+    public void Adicionar(Pessoa pessoa)
+    {
+        throw new NotImplementedException();
+    }
+};
+
+
+#endregion
